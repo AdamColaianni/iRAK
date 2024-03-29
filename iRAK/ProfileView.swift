f89b287d0da1a56b4697f91fa448a9edec7c3a42
AdamColaianni <colaianniadam@gmail.com>
2024-03-29 11:35:49 -0500

    Created ProfileView.swift

---
+++ iRAK/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  iRAK
+//
+//  Created by 90310013 on 3/29/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
