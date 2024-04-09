9811d0ae78533c2902fab078d9c229dc99fad2ee
Raghava-Dwivedi <raghava.p.dwivedi@gmail.com>
2024-04-09 10:46:20 -0500

    Created HumanBenchmarkView

---
+++ iRAK/HumanBenchmarkView.swift
@@ -0,0 +1,20 @@
+//
+//  HumanBenchmarkView.swift
+//  iRAK
+//
+//  Created by 64016641 on 4/9/24.
+//
+
+import SwiftUI
+
+struct HumanBenchmarkView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HumanBenchmarkView_Previews: PreviewProvider {
+    static var previews: some View {
+        HumanBenchmarkView()
+    }
+}
