ee7a7d3448eda249b608a5c75417cc5b8b725c5e
Raghava-Dwivedi <raghava.p.dwivedi@gmail.com>
2024-04-15 10:34:00 -0500

    Added HumanBenchmarkStats File
    Added HumanBenchmarkStats File

---
+++ iRAK/HumanBenchmarkStats.swift
@@ -0,0 +1,20 @@
+//
+//  HumanBenchmarkStats.swift
+//  iRAK
+//
+//  Created by 64016641 on 4/15/24.
+//
+
+import SwiftUI
+
+struct HumanBenchmarkStats: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HumanBenchmarkStats_Previews: PreviewProvider {
+    static var previews: some View {
+        HumanBenchmarkStats()
+    }
+}
